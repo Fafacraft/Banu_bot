a741d0bde5a3aecb943ed83e329bb9f098181439
Fafacraft <fafa24252425@yahoo.com>
2023-12-25 01:34:27 +0100

    wraps works but text is small when wrapping

--- utils/image.py
+++ utils/image.py
@@ -7,6 +7,8 @@ def makeBanuTextImg(msg):
     # IMPORTANT : for RAQM to works, you need libraqm installed.  ( https://stackoverflow.com/questions/57545244/installing-raqm-libraqm-windows-10 )
     font = ImageFont.truetype(font='./Banu-Regular.otf', size=65, layout_engine=ImageFont.Layout.RAQM)
     text = msg
+    textLines = textwrap.TextWrapper(width = 60).wrap(text)
+    textLinesNb = len(textLines)
     
     # gets left, top, right, bottom (bounding box of the text)
     corners = font.getbbox(text)
@@ -14,12 +16,14 @@ def makeBanuTextImg(msg):
     size = (corners[2]- corners[0], corners[3]-corners[1])
 
     # make the image the size of the text, with a bit more pixels/room
-    img = Image.new('RGBA', (size[0]+20, size[1]+20), (255, 0, 0, 0))
+    img = Image.new('RGBA', (size[0]+20, size[1]*textLinesNb+20), (255, 0, 0, 0))
 
     # draw the image from the empty image we made, plus the text
     draw = ImageDraw.Draw(im=img)
-    
-    draw.text(xy=(0, 0), text=text, font=font, fill='#FFFFFF')
-
+    offset = 0
+    for line in textLines:
+        draw.text(xy=(0, offset), text=line, font=font, fill='#FFFFFF') 
+        offset += 65
 
     return img
+
