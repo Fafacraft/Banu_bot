8b7b0ed0a05518fdc29df837a28b4a5bbf81e44d
Fafacraft <fafa24252425@yahoo.com>
2023-12-25 01:13:00 +0100

    need text wrapping

--- utils/image.py
+++ utils/image.py
@@ -1,14 +1,25 @@
+import textwrap
 from PIL import Image, ImageDraw, ImageFont
 
 def makeBanuTextImg(msg):
     # Open image
-    img = Image.open(fp='./background.png', mode='r')
 
     # IMPORTANT : for RAQM to works, you need libraqm installed.  ( https://stackoverflow.com/questions/57545244/installing-raqm-libraqm-windows-10 )
     font = ImageFont.truetype(font='./Banu-Regular.otf', size=65, layout_engine=ImageFont.Layout.RAQM)
     text = msg
+    
+    # gets left, top, right, bottom (bounding box of the text)
+    corners = font.getbbox(text)
+    # get the size of the image we will need
+    size = (corners[2]- corners[0], corners[3]-corners[1])
 
+    # make the image the size of the text, with a bit more pixels/room
+    img = Image.new('RGBA', (size[0]+20, size[1]+20), (255, 0, 0, 0))
+
+    # draw the image from the empty image we made, plus the text
     draw = ImageDraw.Draw(im=img)
-
+    
     draw.text(xy=(0, 0), text=text, font=font, fill='#FFFFFF')
+
+
     return img
